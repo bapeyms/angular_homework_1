0681a82d87757fc4478596f9e90f826153bbcf6f
Katherine <katerynaunik@gmail.com>
2026-05-01 00:54:34 +0300

    hw

--- src/app/cv-item/cv-item.ts
+++ src/app/cv-item/cv-item.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-cv-item',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './cv-item.html',
   styleUrl: './cv-item.css',
 })
